bf90cdd940a17dc87fde803cbbfde79dc3eecba3
Charles Brookover <55162055+CharlesBrookover@users.noreply.github.com>
2022-04-30 22:00:32 -0500

    add some absolute paths to the tsconfig.json so make things a little easier to import

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.scss';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Items from './pages/Items';
-import NoMatch from './pages/NoMatch';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Items from './Pages/Items';
+import NoMatch from './Pages/NoMatch';
 
 function App() {
   return (
       <BrowserRouter>
         <div className="App">
-          <Header />
+          <Header/>
           <main>
-            <div className="row">
-              <div className="mx-auto col-10">
-                <Routes>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="items" element={<Items/>}/>
-                  <Route path="*" element={<NoMatch/>}/>
-                </Routes>
+            <div className="container">
+              <div className="row">
+                <div className="mx-auto col">
+                  <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="items" element={<Items/>}/>
+                    <Route path="*" element={<NoMatch/>}/>
+                  </Routes>
+                </div>
               </div>
             </div>
           </main>
           <footer>
-            <p>&copy; 2022 Charles Brookover</p>
+            <div className="d-flex justify-content-end">
+              <p className="p-3">&copy; 2022 Charles Brookover</p>
+            </div>
           </footer>
         </div>
       </BrowserRouter>
